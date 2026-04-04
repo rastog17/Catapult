6e977f4b38567e06cc6399074d7cc0794b4adba8
rastog17 <anikarastogi05@gmail.com>
2026-04-04 17:20:08 -0400

    Refactor doomscroll_feed to use a copy of trials
    Create a safe copy of trials before shuffling and updating scores.

--- Catapult Project/doomscroll.py
+++ Catapult Project/doomscroll.py
@@ -1,15 +1,17 @@
 import random
 
-
 def doomscroll_feed(trials):
 
-    random.shuffle(trials)
+    # SAFE COPY (correct place)
+    trials_copy = [trial.copy() for trial in trials]
 
-    for trial in trials:
+    random.shuffle(trials_copy)
+
+    for trial in trials_copy:
         trial["discovery_score"] = random.randint(60, 100)
 
     return sorted(
-        trials,
+        trials_copy,
         key=lambda x: x["discovery_score"],
         reverse=True
     )
