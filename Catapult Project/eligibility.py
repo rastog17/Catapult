631976bfe8f931870b25594c083ae7e79037f4f3
rastog17 <anikarastogi05@gmail.com>
2026-04-04 16:30:50 -0400

    Edited eligibility.py

--- Catapult Project/eligibility.py
+++ Catapult Project/eligibility.py
@@ -1,14 +1,23 @@
-def eligibility_score(patient):
+def parse_age(age_str):
+    if not age_str:
+        return 0
+    return int(age_str.split()[0])
+
+
+def eligibility_score(user, trial):
 
     score = 0
 
-    if patient["age"] >= 18:
+    min_age = parse_age(trial.get("min_age"))
+    max_age = parse_age(trial.get("max_age"))
+
+    if min_age <= user.get("age", 0) <= max_age:
         score += 40
 
-    if patient["treatments"]:
+    if user.get("condition") in trial.get("conditions", []):
         score += 30
 
-    if patient["condition"]:
+    if user.get("treatments"):
         score += 30
 
     return score
